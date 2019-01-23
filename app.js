f1b05a90e90f4c9c4868baee0112e5cb4c3a643c
Andrew Roska <andrew.l.roska@gmail.com>
2019-01-22 22:27:49 -0800

    Adds results chart to the bottom of the page.

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 'use strict';
 
-var products = [];
+var resultsChart;
+var choices = document.getElementById('product-choices');
+var results = document.getElementById('results-list');
+var allProducts = [];
 var randomProducts = [];
 var totalClicks = 0;
-var choices = document.getElementById('product-choices');
-var results = document.getElementById('results');
+var productNames = [];
+var productClicks = [];
+var productViews = [];
 
 function Product(name) {
   this.filepath = `img/${name}.jpg`;
   this.name = name;
   this.views = 0;
   this.clicks = 0;
-  products.push(this);
+  allProducts.push(this);
 }
 
 new Product('bag');
@@ -40,56 +44,90 @@ function getRandomProducts() {
     if(randomProducts.length > 5) {
       randomProducts.pop();
     }  
-    var random = Math.floor(Math.random() * products.length);
+    var random = Math.floor(Math.random() * allProducts.length);
     if(!randomProducts.includes(random)) {
       randomProducts.unshift(random);
       i++;
     }
   }
 }
+
 function showRandomProducts() {
   var img;
   choices.innerHTML = '';
   for(var i = 0; i < 3; i++) {
     img = document.createElement('img');
-    img.src = products[randomProducts[i]].filepath;
-    img.alt = products[randomProducts[i]].name;
-    img.title = products[randomProducts[i]].name;
-    products[randomProducts[i]].views++;
+    img.src = allProducts[randomProducts[i]].filepath;
+    img.alt = allProducts[randomProducts[i]].name;
+    img.title = allProducts[randomProducts[i]].name;
+    allProducts[randomProducts[i]].views++;
     choices.appendChild(img);
   }    
 }
 
-
 function printResults() {
-  var li;
+  results.hidden = true;
   results.textContent = "Results:";
-  for(var i = 0; i < products.length; i++) {
-    li = document.createElement('li');
-    li.textContent = `${products[i].clicks} votes for ${products[i].name}`;
-    results.appendChild(li);
+  for(var i = 0; i < allProducts.length; i++) {
+    var liEl = document.createElement('liEl');
+    liEl.textContent = `${allProducts[i].clicks} votes for ${allProducts[i].name}`;
+    results.appendChild(liEl);
+  }
+}
+
+function updateChartArrays() {
+  for(var i = 0; i < allProducts.length; i++) {
+    productNames.push(allProducts[i].name);
+    productClicks.push(allProducts[i].clicks);
+    productViews.push(allProducts[i].views);
   }
 }
 
 function handleClick(event) {
-  for(var i = 0; i < products.length; i++)
-  if (event.target.alt === products[i].name) {
-    products[i].clicks++;
-    totalClicks++;
-    getRandomProducts();
-    showRandomProducts();
+  for(var i = 0; i < allProducts.length; i++) {
+
+    if (event.target.alt === allProducts[i].name) {
+      allProducts[i].clicks++;
+      totalClicks++;
+
+      if(totalClicks === 25 ) {
+        choices.removeEventListener('click', handleClick);
+        updateChartArrays();
+        printResults();
+        drawChart();
+        return;
+      }
+  
+      getRandomProducts();
+      showRandomProducts();
+    }
+
+    if(event.target.alt === undefined) {
+      return alert('Please click on an image');
+    }
   }
-  if(event.target.alt === undefined) {
-    return alert('Please click on an image');
-  }
-  if(totalClicks === 25) {
-    choices.removeEventListener('click', handleClick);
-    printResults();
-  }
+
 }
 
-choices.addEventListener('click', handleClick);
+var data = {
+  labels: productNames,
+  datasets: [{
+    label: 'Results',
+    data: productClicks,
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+  }]
+};
 
+function drawChart(){
+  var ctx = document.getElementById('results').getContext('2d');
+  resultsChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+  });
+}
 
 getRandomProducts();
 showRandomProducts();
+
+choices.addEventListener('click', handleClick);
